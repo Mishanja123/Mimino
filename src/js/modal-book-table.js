eaf88cddfdc0e3b2ede0b2e8f204a5044348bc6e
millenialia <lenalebo@gmail.com>
2023-04-05 18:23:24 +0200

    some updates

--- src/js/modal-book-table.js
+++ src/js/modal-book-table.js
@@ -4,11 +4,11 @@
     closeModalBtn: document.querySelector("[data-modal-table-close]"),
     modal: document.querySelector("[data-modal-table]"),
   };
-  
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
   }
+
 })();
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './js/modal-price.js';
 import './js/modal-book-table.js';
-import './js/modal-book-room.js';
+import './js/modal-book-room';
 import './js/mobile-menu.js';
 import './js/map.js';
