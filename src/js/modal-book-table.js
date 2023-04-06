58391db8294bb3e2a8ab696b914da6b51ad5372f
millenialia <lenalebo@gmail.com>
2023-04-06 16:54:28 +0200

    modal room done

--- src/js/modal-book-table.js
+++ src/js/modal-book-table.js
@@ -1,14 +1,15 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-table-open]"),
-    closeModalBtn: document.querySelector("[data-modal-table-close]"),
+    openModalTableBtn: document.querySelector("[data-modal-table-open]"),
+    closeModalTableBtn: document.querySelector("[data-modal-table-close]"),
     modal: document.querySelector("[data-modal-table]"),
   };
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalTableBtn.addEventListener("click", toggleModal);
+  refs.closeModalTableBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+    console.log(refs);
   }
 
 })();
